0fc65f3c3b076aacb7184a81f0e8bda9d569a8dd
shadow-walkere <110094422+shadow-walkere@users.noreply.github.com>
2025-03-03 21:26:16 +0300

    Update variable.cs

--- variable.cs
+++ variable.cs
@@ -1,1 +1,15 @@
+# Data Types
+string, integer, character, decimal, boolean
 
+##Declaring and Initializing variables
+to declare for example int number =3;
+to initialize for example string name = "Joe";
+
+###Retrieving and Setting variables 
+example
+int number=4;
+Console.Write(number);
+
+####Declaring variables implicity 
+use var to do so example 
+  var name = "John";
